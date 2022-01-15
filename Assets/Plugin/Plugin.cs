356b2847af540f3ad063c079ab82f60a538a0b35
Keijiro Takahashi <keijiro@gmail.com>
2022-01-15 14:19:31 +0900

    Error handling

--- Assets/Plugin/Plugin.cs
+++ Assets/Plugin/Plugin.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Runtime.InteropServices;
 
 namespace Qoi {
@@ -22,27 +23,31 @@ public static class Plugin
 
     public static Header DecodeHeader(byte[] data)
     {
-        var size = (System.UIntPtr)data.Length;
+        var size = (UIntPtr)data.Length;
         Qoi.Header header;
-        decode_header(data, size, out header);
+        if (!decode_header(data, size, out header))
+            throw new ArgumentException("Failed to open file.");
         return header;
     }
 
     public static void Decode(byte[] data, byte[] output)
     {
-        var data_size = (System.UIntPtr)data.Length;
-        var output_size = (System.UIntPtr)output.Length;
-        decode(data, data_size, output, output_size);
+        var data_size = (UIntPtr)data.Length;
+        var output_size = (UIntPtr)output.Length;
+        if (!decode(data, data_size, output, output_size))
+            throw new ArgumentException("Failed to decode file.");
     }
 
     [DllImport(_dll)]
-    public static extern void decode_header
-      ([In] byte[] data, System.UIntPtr data_size, out Header output);
+    [return: MarshalAs(UnmanagedType.U1)]
+    public static extern bool decode_header
+      ([In] byte[] data, UIntPtr data_size, out Header output);
 
     [DllImport(_dll)]
-    public static extern void decode
-      ([In] byte[] data, System.UIntPtr data_size,
-       [Out] byte[] output, System.UIntPtr output_size);
+    [return: MarshalAs(UnmanagedType.U1)]
+    public static extern bool decode
+      ([In] byte[] data, UIntPtr data_size,
+       [Out] byte[] output, UIntPtr output_size);
 }
 
 } // namespace Qoi
--- Plugin/src/lib.rs
+++ Plugin/src/lib.rs
@@ -1,12 +1,15 @@
 #[no_mangle]
-pub unsafe extern fn decode_header(data: *const u8, data_size: usize, output: *mut rapid_qoi::Qoi) {
+pub unsafe extern fn decode_header(data: *const u8, data_size: usize, output: *mut rapid_qoi::Qoi) -> bool {
     let src = std::slice::from_raw_parts(data, data_size);
-    *output = rapid_qoi::Qoi::decode_header(src).unwrap();
+    let res = rapid_qoi::Qoi::decode_header(src);
+    if res.is_err() { return false; }
+    *output = res.unwrap();
+    true
 }
 
 #[no_mangle]
-pub unsafe extern fn decode(data: *const u8, data_size: usize, output: *mut u8, output_size: usize) {
+pub unsafe extern fn decode(data: *const u8, data_size: usize, output: *mut u8, output_size: usize) -> bool {
     let src = std::slice::from_raw_parts(data, data_size);
     let dst = std::slice::from_raw_parts_mut(output, output_size);
-    rapid_qoi::Qoi::decode(src, dst).unwrap();
+    rapid_qoi::Qoi::decode(src, dst).is_ok()
 }
